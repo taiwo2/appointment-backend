532964d08e596659f7330b5d747caaa33c26ed67
taiwo2 <adetonat@ymail.com>
2021-07-12 18:44:12 +0100

    Add user_spec.rb content

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'expect user to be invalid without any details' do
+    expect(User.new).to_not be_valid
+  end
+
+  it 'expect user to be invalid without name' do
+    expect(User.create(email: 'abcd@mail.com', password: '12345')).to_not be_valid
+  end
+
+  it 'expect user to be valid with all parameters' do
+    expect(User.create(name: 'John', email: 'abcd@mail.com', password: '12345')).to be_valid
+  end
+  describe 'associations' do
+    it { should have_many(:appointments) }
+    it { should have_many(:doctors) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+  end
 end
