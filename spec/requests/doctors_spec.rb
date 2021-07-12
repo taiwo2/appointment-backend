13c591f409fd43e969708fcbb13bceb6ac4445da
taiwo2 <adetonat@ymail.com>
2021-07-12 18:47:47 +0100

    Add doctors_spec.rb request folder content

--- spec/requests/doctors_spec.rb
+++ spec/requests/doctors_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "Doctors", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Doctors', type: :request do
+  it 'unauthorized to see doctors without logging in' do
+    get '/api/v1/doctors'
+    expect(response).to have_http_status(:unauthorized)
+  end
+
+  it 'renders list of doctors only for logged in user' do
+    post '/login', params:
+      {
+        email: 'abcdef@mail.com',
+        password: '12345678'
+      }
+    headers = { 'Authorization': JSON.parse(response.body)['jwt'] }
+    get '/api/v1/doctors', headers: headers
+    expect(response).to have_http_status(:ok)
+    expect(response.content_type).to eq('application/json')
   end
 end
