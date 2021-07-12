4e86605a4a5645d9299b7f46a4d97b9fe56c39fd
taiwo2 <adetonat@ymail.com>
2021-07-12 18:17:53 +0100

    Add database contents

--- db/migrate/20210712171255_create_appointments.rb
+++ db/migrate/20210712171255_create_appointments.rb
@@ -1,7 +1,9 @@
 class CreateAppointments < ActiveRecord::Migration[6.1]
   def change
     create_table :appointments do |t|
-
+      t.belongs_to :doctor
+      t.belongs_to :user
+      t.datetime :appointment_date
       t.timestamps
     end
   end
--- db/migrate/20210712171355_create_doctors.rb
+++ db/migrate/20210712171355_create_doctors.rb
@@ -1,7 +1,10 @@
 class CreateDoctors < ActiveRecord::Migration[6.1]
   def change
     create_table :doctors do |t|
-
+      t.string :name
+      t.string :qualification
+      t.string :experience
+      t.string :image
       t.timestamps
     end
   end
